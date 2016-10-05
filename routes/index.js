7cc7960e8559748005b8e19af1f6c0f488ba86de
Mark Chipp <mark.chipp@live.ca>
2016-10-04 20:15:43 -0400

    index comments added

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mark Chipp' });
 });
 
+/*GET about me page*/
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me'});
 });
 
+/*GET projects page*/
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects'});
 });
 
+/*GET serices page*/
 router.get('/services', function(req, res, next) {
   res.render('services', { title: 'Services'});
 });
 
+/*GET contact me page*/
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me'});
 });
