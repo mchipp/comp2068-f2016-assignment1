29526d778412aa0c6b58d441f06a00c9a3ffb886
Mark Chipp <mark.chipp@live.ca>
2016-10-04 20:10:15 -0400

    addition of services and contact me views

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,12 @@ router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects'});
 });
 
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me'});
+});
+
 module.exports = router;
