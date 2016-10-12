74ba404cec33bb0b1ef226037242ea88a67179d0
Mark Chipp <mark.chipp@live.ca>
2016-10-12 12:15:37 -0400

    adding mailer form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var bodyparser = require('body-parser');
 
 var routes = require('./routes/index');
 var about = require('./routes/about');
@@ -13,17 +15,49 @@ var contact = require('./routes/contact');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/contact', function (req, res, next) {
+  var mailOpts, smtpTrans;
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+
+smtpTrans = nodemailer.createTransport('smtps://chipp.mark@gmail.com:Omicron158@smtp.gmail.com');
+
+  //Mail options
+  mailOpts = {
+      //from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
+      //from: ''"Fred Foo"'', //grab form data from the request body object
+      to: 'chipp.mark@gmail.com',
+      subject: 'Website contact form',
+      text: req.body.name + " " + req.body.email + " " + req.body.message
+  };
+
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+          res.render('contact', { title: 'Contact Me', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
+      }
+      //Yay!! Email sent
+      else {
+          res.render('contact', { title: 'Contact Me', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+      }
+  });
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', routes);
 app.use('/about', about);
--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /*GET contact me page*/
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me'});
+  res.render('contact', { title: 'Contact Me',
+                          page: 'contact'});
 });
 
 module.exports = router;
